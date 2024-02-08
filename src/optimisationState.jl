32b5c9fbab5ae3c6e8a0fe8385d41b07c6226039
Thomas Burton <tburton5572@gmail.com>
2024-02-08 16:08:14 +0000

    implemented nelder mead optimisation state

--- src/optimisationState.jl
+++ src/optimisationState.jl
@@ -3,12 +3,21 @@
 abstract type OptimisationState end
 
 struct FirstOrderOptimisationState <: OptimisationState
+    iteration::Int
     objectiveValue::Float64
     gradientNorm::Float64
-    iteration::Int
     time::Float64
     stepSize::Float64
 end
 Base.show(io::IO, x::FirstOrderOptimisationState) = @printf io "|%10d   |   %5.2e  |  %5.5e  |  %5.5e  |" x.iteration x.stepSize x.objectiveValue x.gradientNorm
+Base.convert(::Type{FirstOrderOptimisationState}, state::Optim.OptimizationState{<:Any, <:Optim.FirstOrderOptimizer}, prevIteration) = FirstOrderOptimisationState(state.value, state.g_norm, prevIteration + 1, state.metadata["time"], state.metadata["Current step size"])
 
-struct NelderMeadOptimisationState <: OptimisationState end
+struct NelderMeadOptimisationState <: OptimisationState
+    iteration::Int
+    objectiveValue::Float64
+    gradientNorm::Float64
+    time::Float64
+    stepType::String
+end
+Base.show(io::IO, x::NelderMeadOptimisationState) = @printf io "|%10d   |   %-20s  |  %5.5e  |  %5.5e  |" x.iteration x.stepType x.objectiveValue x.gradientNorm
+Base.convert(::Type{FirstOrderOptimisationState}, state::Optim.OptimizationState{<:Any, <:Optim.FirstOrderOptimizer}, prevIteration) = FirstOrderOptimisationState(state.value, state.g_norm, prevIteration + 1, state.metadata["time"], state.metadata["step_type"])
--- test/test_optimisationState.jl
+++ test/test_optimisationState.jl
@@ -1,12 +1,25 @@
 @testset "First Order Optimisation State" begin
-    # initialise random variables
+    # initialise rndom variables
+    iter = rand(0:99999999)
     value = rand()
     gnorm = rand()
-    iter = rand(0:99999999)
     time = rand()
     stepsize = rand()
 
-    state = OptimWrapper.FirstOrderOptimisationState(value, gnorm, iter, time, stepsize)
+    state = OptimWrapper.FirstOrderOptimisationState(iter, value, gnorm, time, stepsize)
 
     @test repr(state) == "|"*@sprintf("%10d", iter)*"   |   "*@sprintf("%.2e", stepsize)*"  |  "*@sprintf("%.5e", value)*"  |  "*@sprintf("%.5e", gnorm)*"  |"
+end
+
+@testset "Nelder-Mead Optimisation State" begin
+    # initialise rndom variables
+    iter = rand(0:99999999)
+    value = rand()
+    gnorm = rand()
+    time = rand()
+    stepType = rand(["expansion", "reflection", "outside contraction", "inside contraction", "shrink"])
+
+    state = OptimWrapper.NelderMeadOptimisationState(iter, value, gnorm, time, stepType)
+
+    @test repr(state) == "|"*@sprintf("%10d", iter)*"   |   "*@sprintf("%-20s", stepType)*"  |  "*@sprintf("%.5e", value)*"  |  "*@sprintf("%.5e", gnorm)*"  |"
 end
