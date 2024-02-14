fa44c9497412e7a1a3a41c9f64c4c90db4eb86a6
Thomas Burton <tburton5572@gmail.com>
2024-02-14 17:19:23 +0000

    implemented default empty constructor on abstract optimisation state

--- src/optimisationState.jl
+++ src/optimisationState.jl
@@ -1,8 +1,12 @@
 # Definitions of the optimisation state for each iteration.
 
-abstract type OptimisationState end
+abstract type AbstractOptimisationState end
 
-struct GenericOptimisationState <: OptimisationState
+# TODO: there's gotta be a better way? Look at the data flow of the optimisation initialisation.
+(f::Type{<:AbstractOptimisationState})() = f((one(f.types[i]) for i in eachindex(fieldnames(f)))...)
+
+
+struct GenericOptimisationState <: AbstractOptimisationState
     iteration::Int
     objectiveValue::Float64
     time::Float64
@@ -10,7 +14,8 @@ end
 Base.show(io::IO, x::GenericOptimisationState) = @printf io "|%10d   |  %5.5e  |  %5.5e  |" x.iteration x.time x.objectiveValue
 Base.convert(::Type{GenericOptimisationState}, state, prevIteration) = GenericOptimisationState(prevIteration + 1, state.value, state.metadata["time"])
 
-struct FirstOrderOptimisationState <: OptimisationState
+
+struct FirstOrderOptimisationState <: AbstractOptimisationState
     iteration::Int
     objectiveValue::Float64
     gradientNorm::Float64
@@ -20,7 +25,8 @@ end
 Base.show(io::IO, x::FirstOrderOptimisationState) = @printf io "|%10d   |  %5.5e  |   %5.2e  |  %5.5e  |  %5.5e  |" x.iteration x.time x.stepSize x.objectiveValue x.gradientNorm
 Base.convert(::Type{FirstOrderOptimisationState}, state, prevIteration) = FirstOrderOptimisationState(prevIteration + 1, state.value, state.g_norm, state.metadata["time"], state.metadata["Current step size"])
 
-struct NelderMeadOptimisationState <: OptimisationState
+
+struct NelderMeadOptimisationState <: AbstractOptimisationState
     iteration::Int
     objectiveValue::Float64
     gradientNorm::Float64
--- test/test_optimisationState.jl
+++ test/test_optimisationState.jl
@@ -8,6 +8,7 @@
     dummyState = DummyState(iter, value, gnorm, Dict("time"=>time))
     state = OptimWrapper.GenericOptimisationState(iter, value, time)
 
+    @test repr(OptimWrapper.GenericOptimisationState()) == "|"*@sprintf("%10d", 1)*"   |  "*@sprintf("%.5e", 1)*"  |  "*@sprintf("%.5e", 1)*"  |"
     @test repr(state) == "|"*@sprintf("%10d", iter)*"   |  "*@sprintf("%.5e", time)*"  |  "*@sprintf("%.5e", value)*"  |"
     @test convert(OptimWrapper.GenericOptimisationState, dummyState, 5) == OptimWrapper.GenericOptimisationState(6, value, time)
 end
@@ -23,6 +24,7 @@ end
     dummyState = DummyState(iter, value, gnorm, Dict("time"=>time, "Current step size"=>stepSize))
     state = OptimWrapper.FirstOrderOptimisationState(iter, value, gnorm, time, stepSize)
 
+    @test repr(OptimWrapper.FirstOrderOptimisationState()) == "|"*@sprintf("%10d", 1)*"   |  "*@sprintf("%.5e", 1)*"  |   "*@sprintf("%.2e", 1)*"  |  "*@sprintf("%.5e", 1)*"  |  "*@sprintf("%.5e", 1)*"  |"
     @test repr(state) == "|"*@sprintf("%10d", iter)*"   |  "*@sprintf("%.5e", time)*"  |   "*@sprintf("%.2e", stepSize)*"  |  "*@sprintf("%.5e", value)*"  |  "*@sprintf("%.5e", gnorm)*"  |"
     @test convert(OptimWrapper.FirstOrderOptimisationState, dummyState, 5) == OptimWrapper.FirstOrderOptimisationState(6, value, gnorm, time, stepSize)
 end
@@ -38,6 +40,7 @@ end
     dummyState = DummyState(iter, value, gnorm, Dict("time"=>time, "step_type"=>stepType))
     state = OptimWrapper.NelderMeadOptimisationState(iter, value, gnorm, time, stepType)
 
+    @test repr(OptimWrapper.NelderMeadOptimisationState()) == "|"*@sprintf("%10d", 1)*"   |  "*@sprintf("%.5e", 1)*"  |   "*@sprintf("%-20s", "")*"  |  "*@sprintf("%.5e", 1)*"  |  "*@sprintf("%.5e", 1)*"  |"
     @test repr(state) == "|"*@sprintf("%10d", iter)*"   |  "*@sprintf("%.5e", time)*"  |   "*@sprintf("%-20s", stepType)*"  |  "*@sprintf("%.5e", value)*"  |  "*@sprintf("%.5e", gnorm)*"  |"
     @test convert(OptimWrapper.NelderMeadOptimisationState, dummyState, 5) == OptimWrapper.NelderMeadOptimisationState(6, value, gnorm, time, stepType)
 end
