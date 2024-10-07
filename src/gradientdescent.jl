cc87e72d6bc28b5763f57367070ae7aa8c4685c0
Thomas Burton <tburton5572@gmail.com>
2024-10-07 16:21:06 +0100

    altered gradient descent to use new assumed objective signature

--- src/gradientdescent.jl
+++ src/gradientdescent.jl
@@ -1,20 +1,21 @@
 # Definitions for a simple gradient descent as a fall-back when Optim.jl routines throw a little fit.
 
 function gd!(a, objective, step_size::Float64; rtol::Float64=1e-12, maxiter::Real=Inf, verbose::Bool=true, n_it_print::Int=1, trace::Trace=Trace(GradientDescentState), print_io::IO=stdout, callback=(x,i)->false)
+    grad = similar(a)
     verbose ? printHeader(print_io, GradientDescentState) : nothing
     iter = getFinalIteration(trace)
     if !isempty(trace.stateVector)
         maxiter = maxiter + trace[end].iteration
         verbose ? println(print_io, trace[end]) : nothing
     end
-    R = objective(a)[2]
+    R = objective(a)[1]
     while R > rtol && iter < maxiter
         iter += 1
-        R = objective(a)[2]
-        push!(trace.stateVector, convert(GradientDescentState, iter, R, norm(objective.out)))
+        R = objective(grad, a)[1]
+        push!(trace.stateVector, convert(GradientDescentState, iter, R, norm(grad)))
         verbose && iter % n_it_print == 0 ? println(print_io, trace[end]) : nothing
         callback(a, iter) ? break : nothing
-        update!(a, objective.out, step_size)
+        update!(a, grad, step_size)
     end
     return a, trace
 end
--- test/test_gradientdescent.jl
+++ test/test_gradientdescent.jl
@@ -1,18 +1,16 @@
 @testset "Gradient Descent              " begin
     # define rosenbrock function
-    struct RosenbrokGrad
-        out::Vector{Float64}
-    end
-    function (f::RosenbrokGrad)(x)
-        f.out[1] = -2.0 * (1.0 - x[1]) - 400.0 * (x[2] - x[1]^2) * x[1]
-        f.out[2] = 200.0 * (x[2] - x[1]^2)
+    function Rosenbrok(grad, x)
+        grad[1] = -2.0 * (1.0 - x[1]) - 400.0 * (x[2] - x[1]^2) * x[1]
+        grad[2] = 200.0 * (x[2] - x[1]^2)
         F = (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
-        return f.out, F
+        return F
     end
+    Rosenbrok(x) = (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
 
     # perform optimisation
-    x, trace = gd!([0.0, 0.0], RosenbrokGrad([0.0, 0.0]), 1e-3, verbose=false, maxiter=10)
-    gd!(x, RosenbrokGrad([0.0, 0.0]), 1e-3, verbose=false, trace=trace)
+    x, trace = gd!([0.0, 0.0], Rosenbrok, 1e-3, verbose=false, maxiter=10)
+    gd!(x, Rosenbrok, 1e-3, verbose=false, trace=trace)
 
     @test x ≈ [1.0, 1.0] rtol=1e-5
     @test trace.iteration == collect(0:trace[end].iteration)
